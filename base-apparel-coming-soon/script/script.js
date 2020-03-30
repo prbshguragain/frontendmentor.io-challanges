57a0d41b4f5eacfe350e91263efeabf8e012aa5f
Prabesh Guragain <prbshguragain@gmail.com>
2020-03-30 18:14:21 +0545

    Email Validation completed :)

---
+++ base-apparel-coming-soon/script/script.js
@@ -0,0 +1,17 @@
+function validate(){
+    var getText = document.getElementById("text1").value;
+    var regX = /^([a-z0-9\.-]+)@([a-z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+    
+    if (regX.test(getText)){
+        document.getElementById("error").style.visibility="hidden";
+        document.getElementById("erroricon").style.visibility="hidden";
+        document.getElementById("text1").style.border="1px solid hsl(0, 74%, 74%)";
+        alert(`Your email ${getText} has been registered`);
+    }
+
+    else{
+        document.getElementById("error").style.visibility="visible";
+        document.getElementById("erroricon").style.visibility="visible";
+        document.getElementById("text1").style.border="2px solid hsl(0, 74%, 74%)";
+    }
+}
